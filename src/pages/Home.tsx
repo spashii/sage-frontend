394ef979eaa1b926edb87155e335f55e5f33c133
spashii <sameer.pashikanti@gmail.com>
2021-04-06 23:37:18 +0530

    initialize routing

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import { Link, RouteComponentProps } from '@reach/router';
+
+function Home(_props: RouteComponentProps) {
+	return (
+		<div>
+			the main page w/ all the listings and shit
+			<div>
+				<ul>
+					<li>
+						<Link to="/">to login</Link>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
